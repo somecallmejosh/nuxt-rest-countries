3b223a18736e9f218fdbb38eb7efa0a8b270725d
somecallmejosh <josh@thebrileys.com>
2025-01-28 14:21:46 -0500

    App template layout with color mode toggle.

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="font-custom text-wood-smoke dark:text-white dark:bg-mine-shaft">
+  <div class="font-custom ">
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <div class="text-wood-smoke bg-alabaster dark:text-white dark:bg-mine-shaft min-h-dvh flex flex-col ">
+      <layout-app-header />
+      <main class="wrapper flex-1">
+        <NuxtPage />
+      </main>
+    </div>
   </div>
 </template>
+<style>
+  .wrapper {
+    @apply w-full max-w-[80rem] mx-auto px-4 lg:px-0;
+  }
+</style>
