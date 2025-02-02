7a311556afbf07be4584870675d959a19c4b9630
somecallmejosh <josh@thebrileys.com>
2025-02-02 15:56:16 -0500

    Create filter component and associated UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
+    '@vueuse/nuxt',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     '@nuxtjs/seo',
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,33 @@
 <script setup>
 const route = useRoute()
-const url = route.query
-  ? `/api/countries?${Object.keys(route.query)
-      .map((key) => `${key}=${route.query[key]}`)
-      .join('&')}`
-  : '/api/countries'
-const { data } = await useFetch(url)
+
+const url = computed(() => {
+  if (!route.query) return '/api/countries'
+  return `/api/countries?${Object.keys(route.query)
+    .map(key => `${key}=${route.query[key]}`)
+    .join('&')}`
+})
+
+const { data, pending, error } = await useFetch(url)
+
+// watch route params, and refetch data when they change
+watchEffect(() => {
+  const url = route.query
+    ? `/api/countries?${Object.keys(route.query)
+        .map((key) => `${key}=${route.query[key]}`)
+        .join('&')}`
+    : '/api/countries'
+  useFetch(url)
+})
 </script>
 <template>
   <div class="py-8 lg:py-12 space-y-6 lg:space-y-12">
+    <section aria-label="Search and filters" class="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+      <div></div>
+      <div>
+        <filters-regions />
+      </div>
+    </section>
     <ul class="grid lg:grid-cols-4 lg:gap-12 gap-6">
       <Card v-for="country in data" :key="country.name" :country="country" />
     </ul>
