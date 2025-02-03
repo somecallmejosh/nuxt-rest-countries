7ea4f3ad88730add9303398ae89583f257da3b4c
somecallmejosh <josh@thebrileys.com>
2025-02-03 16:39:06 -0500

    Update drop down menu colors and functionality.

--- components/filters/regions.vue
+++ components/filters/regions.vue
@@ -1,34 +1,47 @@
 <script setup>
 const route = useRoute()
 import { OnClickOutside } from '@vueuse/components'
+import { watch } from 'vue'
 const { data: regions } = await useFetch('/api/regions')
-const region = ref(route.query.region ? route.query.region : 'All')
+const region = ref(route.query.region ? route.query.region : 'Filter By Region')
 const regionMenuVisible = ref(false)
-function showRegions() {
-  regionMenuVisible.value = true
+function toggleRegionMenu() {
+  regionMenuVisible.value = !regionMenuVisible.value
 }
 function hideRegions() {
   regionMenuVisible.value = false
 }
+
+watch(() => route.query.region, (newRegion) => {
+  region.value = newRegion ? newRegion : 'All'
+})
 </script>
 <template>
   <div class="relative inline-flex">
-    <button @click="showRegions" class="box-shadow" aria-haspopup="true" aria-expanded="false" aria-controls="region-filter">
-      <span>{{ region }}</span>
-      <icons-chevron-down />
-    </button>
     <OnClickOutside @trigger="hideRegions">
+      <button @click="toggleRegionMenu" class="box-shadow" aria-haspopup="true" aria-expanded="false" aria-controls="region-filter">
+        <span>{{ region }}</span>
+        <icons-chevron-down />
+      </button>
+
       <ul
         v-if="regionMenuVisible"
         id="region-filter"
         role="menu"
         tabindex="-1"
-        class="bg-white box-shadow rounded-[0.3125rem] inline-flex flex-col absolute top-[3.2rem] left-0 z-20 px-[1.5rem] py-4 w-full text-sm" name="region-filter">
-        <li v-for="region in regions" :key="region">
-          <NuxtLink :to="'/?region=' + region" class="py-1 block">{{ region }}</NuxtLink>
+        class="bg-white box-shadow rounded-[0.3125rem] inline-flex flex-col absolute top-[3.2rem] left-0 z-20 px-[1.5rem] py-4 w-full text-sm dark:bg-ebony-clay" name="region-filter">
+        <li v-for="item in regions" :key="item">
+          <NuxtLink :to="'/?region=' + item"
+          >
+            {{ item }}
+            <span v-if="item === region"><icons-back-arrow /></span>
+          </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/" class="py-1 block">All</NuxtLink>
+          <NuxtLink to="/" class="py-1 block">
+            All
+            <span v-if="region === 'All'"><icons-back-arrow /></span>
+          </NuxtLink>
         </li>
       </ul>
     </OnClickOutside>
@@ -41,5 +54,11 @@ button {
 
 .box-shadow {
   box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.05);
+}
+a {
+  @apply -mx-4 px-4 rounded-[0.3125rem] py-1 flex items-center gap-1 justify-between;
+}
+a:hover, a:focus {
+  @apply bg-ebony-clay/5 dark:bg-white/5;
 }
 </style>
